e5d23d3207160fa58b17aae3649993c1ea8f34dc
Victor <olowofesovictor@gmail.com>
2021-05-07 02:28:33 +0100

    Tetris Screen

---
+++ tetris.py
@@ -0,0 +1,33 @@
+
+
+#TODO:
+#Tetris
+#create a tile object
+#create tetrinomes
+#create grid
+#move tetrinome in grid [left, right, down]
+#rotate the tetrinome   
+
+
+import pygame
+
+
+SCREENRECT = pygame.Rect(0, 0, 640, 480)
+
+
+
+
+
+def main()-> int:
+    
+    #init screen
+    screen = pygame.display.set_mode(SCREENRECT.size)
+
+    
+    
+
+
+if __name__ == "__main__":
+    
+    pygame.init
+    exit(main())
